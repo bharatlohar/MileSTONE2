b06074d1921ee1af8da5dee81172ed67d9018881
Bharat Lohar <loharbharat225@gmail.com>
2024-11-16 22:30:11 +0530

    M3

--- budgettracker/utils.py
+++ budgettracker/utils.py
@@ -5,4 +5,4 @@ def send_alert(user, category, exceeded=False):
     subject = f"{'Exceeded' if exceeded else 'Nearing'} Budget Alert"
     message = f"You have {'exceeded' if exceeded else 'neared'} your budget for {category.name}."
     print(f"Preparing to send email to {user.email} with subject: {subject}") 
-    send_mail(subject, message, 'hasim.inf2024@gmail.com', [user.email])
+    send_mail(subject, message, 'bharat.inf2024@gmail.com', [user.email])
